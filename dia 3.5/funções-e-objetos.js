71907f63a4ad3d3a20a3a530ddc7f1971d36e90a
felipesegala <felipe_segala@outlook.com>
2023-05-12 09:27:01 -0300

    treino de funções

--- dia 3.5/funções-e-objetos.js
+++ dia 3.5/funções-e-objetos.js
@@ -7,6 +7,17 @@ const parEimpra = (array) => {
     }
 
     for(let index = 0; index < array.length; index += 1) {
-        
+        if(array[index] % 2 === 0){
+            obj.pares += 1;
+        }else {
+            obj.impares += 1;
+        }
     }
+    return obj;
 }
+console.log(parEimpra([1,2,3,4,5,6,7,8,9]));
+
+
+//array[index] = estou pegando array e passando por todo seus valores:
+//se o numero for par, eu acesso o obj.pares(esse obj é a variavel criada no formato de objeto),(já o pares é a chave dentro do obj) e adiciono mais 1 nos pares;
+// caso seja impar eu acesso obj.impares)que funciona da mesma forma do anterior, e adiciono nos impares:
