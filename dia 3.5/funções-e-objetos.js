df327d71c1b2c004766c4ac991c148ccf369d638
felipesegala <felipe_segala@outlook.com>
2023-05-12 09:17:13 -0300

    dias de mentoria

---
+++ dia 3.5/funções-e-objetos.js
@@ -0,0 +1,12 @@
+// Crie uma função que dado um array de numeros inteiros, retorne a quantidade de numeros pares e impares no formato de obj:
+
+const parEimpra = (array) => {
+    const obj = {
+        pares: 0,
+        impares: 0,
+    }
+
+    for(let index = 0; index < array.length; index += 1) {
+        
+    }
+}
